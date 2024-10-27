19f572c94ccab66a63cc9d66c24550ce1eb9969a
eduardomj2004 <eduardomj2004@gmail.com>
2024-10-27 15:36:20 +0000

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationteste27/MainActivity2.kt
@@ -0,0 +1,44 @@
+package com.example.myapplicationteste27
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+//Teste TPC2 1 13:30 -15:30
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Log.d("MainActivity", "onCreate")
+        //Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,R.string.app_name, Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,R.string.textopergunta, Toast.LENGTH_SHORT).show()
+    }
+    // Tenho de adicionar o OnPause,OnResume e OnStop
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPause")
+        Toast.makeText(this,R.string.textoOnPause, Toast.LENGTH_SHORT).show()
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume")
+        Toast.makeText(this,R.string.textoOnResume, Toast.LENGTH_SHORT).show()
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStop")
+
+    }
+
+
+}
